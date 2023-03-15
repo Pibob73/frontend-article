642b46f2dda2805f86d5b14ce3f7a20236c93ad0
fe <fefe@lab.com>
2023-03-15 18:28:08 +0300

    .

--- articlesWorker/Server.js
+++ articlesWorker/Server.js
@@ -1,37 +1,38 @@
 "use strict";
 const http = require("http");
-const { Chunk } = require("webpack");
 const fs = require("fs");
 const path = require("path");
 const hostName = "127.0.0.1";
 const port = "3000";
 const requestListener = function (req, res) {
-  const data = "";
-  req.on("data", (chunk) => {
-    data += chunk;
-  });
-  req.on("end", () => {
-    console.log(data);
-  });
-  const article = JSON.parse(data);
-  console.dir({
-    title: article.title,
-    author: article.author,
-    text: article.text,
-  });
-  const FILE_OPTIONS = { encoding: "utf-8" };
-  const INPUT_FILE_PATH = path.resolve(__dirname, "createArticle.html");
-  fs.readFile(INPUT_FILE_PATH, FILE_OPTIONS, (err, contents) => {
-    if (err) {
-      return console.error(err);
-    }
-
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(contents);
-  });
+  res.setHeader("Content-Type", "text/html");
+  res.writeHead(200);
+  if (!!req.url) {
+    let buffer = [];
+    let data = "";
+    req.on("data", (chunk) => {
+      buffer.push(chunk);
+      data += String(chunk);
+    });
+    req.on("end", () => {
+      let resultBuff = Buffer.concat(buffer).toJSON();
+      console.log(resultBuff);
+      console.log(typeof resultBuff);
+      let str = '{"title":"good","author":"good","text":"good"}';
+      if (resultBuff === str) console.log("good");
+      const article = JSON.parse(resultBuff);
+      console.dir({
+        title: article.title,
+        author: article.author,
+        text: article.text,
+      });
+    });
+    fs.readFile("createArticle.html", (err, data) => res.end(data));
+  }
+  if (req.url === "/article/") {
+  }
 };
 const server = http.createServer(requestListener);
 server.listen(port, hostName, () => {
-  console.log("server work at http://${hostName}: ${port}");
+  console.log(`server work at http://${hostName}: ${port}`);
 });
